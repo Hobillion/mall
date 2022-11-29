884112ac0fff4412ad6a76a7673f4aa1d6561ca1
lzp <1608757513@qq.com>
2022-11-29 17:59:44 +0800

    初始化项目创建

--- gulimall-product/src/test/java/com/lzp/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/lzp/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,24 @@
 package com.lzp.gulimall.product;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.lzp.gulimall.product.entity.BrandEntity;
+import com.lzp.gulimall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.annotation.Resource;
+
 @SpringBootTest
 class GulimallProductApplicationTests {
+    @Resource
+    BrandService brandService;
 
     @Test
     void contextLoads() {
+//        BrandEntity brandEntity = new BrandEntity();
+//        brandEntity.setDescript("huawei");
+//        System.out.println(brandService.save(brandEntity));
+        System.out.println(brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1)));
     }
 
 }
